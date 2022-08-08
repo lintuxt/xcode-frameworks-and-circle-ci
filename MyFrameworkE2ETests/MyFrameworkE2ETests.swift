a472fb6c3d3bbbb94fd5587c1ced013a846719fe
Alexis Charytonow <alexis@nodeux.com>
2022-08-08 15:12:11 -0700

    Xcode E2E Tests.

---
+++ MyFrameworkE2ETests/MyFrameworkE2ETests.swift
@@ -0,0 +1,16 @@
+//
+//  MyFrameworkE2ETests.swift
+//  MyFrameworkE2ETests
+//
+//  Created by Alexis Charytonow on 8/8/22.
+//
+
+import XCTest
+
+class MyFrameworkE2ETests: XCTestCase {
+
+    func test_exampleE2ETest() {
+        XCTAssertEqual(true, true)
+    }
+
+}
